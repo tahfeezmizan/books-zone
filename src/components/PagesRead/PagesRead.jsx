d31aad9b2dc84aca48f3e255d700f6b03be82443
tahfeezmizan <mizanahmed5316@gmial.com>
2024-03-28 11:40:01 +0600

    applid data sorting function

--- src/components/PagesRead/PagesRead.jsx
+++ src/components/PagesRead/PagesRead.jsx
@@ -4,51 +4,6 @@ import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
 import { getStoredBooks } from '../utility/localStorage';
 const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-// const data = [
-//     {
-//         name: 'Page A',
-//         uv: 4000,
-//         pv: 2400,
-//         amt: 2400,
-//     },
-//     {
-//         name: 'Page B',
-//         uv: 3000,
-//         pv: 1398,
-//         amt: 2210,
-//     },
-//     {
-//         name: 'Page C',
-//         uv: 2000,
-//         pv: 9800,
-//         amt: 2290,
-//     },
-//     {
-//         name: 'Page D',
-//         uv: 2780,
-//         pv: 3908,
-//         amt: 2000,
-//     },
-//     {
-//         name: 'Page E',
-//         uv: 1890,
-//         pv: 4800,
-//         amt: 2181,
-//     },
-//     {
-//         name: 'Page F',
-//         uv: 2390,
-//         pv: 3800,
-//         amt: 2500,
-//     },
-//     {
-//         name: 'Page G',
-//         uv: 3490,
-//         pv: 4300,
-//         amt: 2100,
-//     },
-// ];
-
 
 const PagesRead = () => {
     const books = useLoaderData();
@@ -76,7 +31,6 @@ const PagesRead = () => {
 
     const TriangleBar = (props) => {
         const { fill, x, y, width, height } = props;
-
         return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
     };
 
--- storedBook-others.jsx
+++ storedBook-others.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredBooks, getStoredWishlist } from "../utility/localStorage";
 import StoredReadBookDetials from "./StoredReadBookDetials";
@@ -7,7 +7,7 @@ const StoredBooks = () => {
     const allBooks = useLoaderData();
     const [readBook, setReadBook] = useState([]);
     const [wishlist, setWishlist] = useState([]);
-    const [sortBy, setSortBy] = useState(""); // State to hold the selected sorting criteria
+    const [sortBy, setSortBy] = useState("");
 
     useEffect(() => {
         const storedBookIds = getStoredBooks();
@@ -23,33 +23,8 @@ const StoredBooks = () => {
             const wishlist = allBooks.filter(wishlist => storedWishListIds.includes(wishlist.bookId));
             setWishlist(wishlist);
         }
+
     }, [allBooks]);
-
-    // Sorting function for read books
-    const sortReadBooks = (criteria) => {
-        let sortedReadBooks = [...readBook];
-        if (criteria === "rating") {
-            sortedReadBooks.sort((a, b) => b.rating - a.rating);
-        } else if (criteria === "pages") {
-            sortedReadBooks.sort((a, b) => a.pages - b.pages);
-        } else if (criteria === "publisherYear") {
-            sortedReadBooks.sort((a, b) => a.publisherYear - b.publisherYear);
-        }
-        setReadBook(sortedReadBooks);
-    };
-
-    // Sorting function for wishlist
-    const sortWishlist = (criteria) => {
-        let sortedWishlist = [...wishlist];
-        if (criteria === "rating") {
-            sortedWishlist.sort((a, b) => b.rating - a.rating);
-        } else if (criteria === "pages") {
-            sortedWishlist.sort((a, b) => a.pages - b.pages);
-        } else if (criteria === "publisherYear") {
-            sortedWishlist.sort((a, b) => a.publisherYear - b.publisherYear);
-        }
-        setWishlist(sortedWishlist);
-    };
 
     return (
         <div className='w-full lg:w-11/12 xl:w-8/12 mx-auto px-4 md:px-6 lg:px-0 py-10'>
@@ -62,9 +37,9 @@ const StoredBooks = () => {
             <div className="flex items-center justify-center dropdown dropdown-bottom">
                 <div tabIndex={0} role="button" className="btn m-1 text-center">Sort By</div>
                 <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a onClick={() => { setSortBy("rating"); sortReadBooks("rating"); sortWishlist("rating"); }}>Rating</a></li>
-                    <li><a onClick={() => { setSortBy("pages"); sortReadBooks("pages"); sortWishlist("pages"); }}>Number of pages</a></li>
-                    <li><a onClick={() => { setSortBy("publisherYear"); sortReadBooks("publisherYear"); sortWishlist("publisherYear"); }}>Publisher year</a></li>
+                    <li><a>Rating</a></li>
+                    <li><a>Number of pages</a></li>
+                    <li><a>Publisher year</a></li>
                 </ul>
             </div>
 
